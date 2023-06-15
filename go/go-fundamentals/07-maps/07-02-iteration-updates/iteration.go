a9a83161682418911d4c515ea6c1e29c7073a8d3
ChaseCondon <chase.condon@gmail.com>
2023-06-15 14:40:49 +0100

    Added section on maps

---
+++ go/go-fundamentals/07-maps/07-02-iteration-updates/iteration.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	testMap := map[string]int{
+		"A": 1,
+		"B": 2,
+		"C": 3,
+		"D": 4,
+		"E": 5,
+		"F": 6,
+		"G": 7,
+		"H": 8,
+		"I": 9,
+	}
+
+	// Ordering is not guaranteed!
+	for mapKey, mapVal := range testMap {
+		fmt.Printf("Key is: %v Value is: %v\n", mapKey, mapVal)
+	}
+
+	testMap["A"] = 100
+	testMap["J"] = 1973
+	fmt.Println(testMap)
+
+	delete(testMap, "J")
+	fmt.Println(testMap)
+}
